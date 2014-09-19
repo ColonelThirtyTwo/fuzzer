687ad96a4bb49f6f8ab3a3c3741d915dbbf488f9
Ryan Switzer <rjs4244@rit.edu>
2014-09-19 17:45:50 -0400

    Fixed command arguments.

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import requests
 import argparse
 
 
-print("fuzz [discover | test] url OPTIONS")
 
-cmds = argparse.ArgumentParser(description='COMMANDS:')
-cmds.add_argument('discover',help='Output a comprehensive,human-readable list of all \
+fuzz = argparse.ArgumentParser(description='fuzz [discover | test] url OPTIONS', add_help=False)
+
+#print('COMMANDS:')
+fuzz.add_argument('discover',help='Output a comprehensive,human-readable list of all \
         discovered inputs to the system. Techniques include both crawling and guessing.')
-cmds.add_argument('test',help='Discover all inputs, then attempt a list of exploit vectors\
+fuzz.add_argument('test',help='Discover all inputs, then attempt a list of exploit vectors\
         on those inputs. Report potential vulnerabilities.')
 
-ops = argparse.ArgumentParser(description='OPTIONS:')
-ops.add_argument('--custom-auth=string',help='Signal that the fuzzer should use hard-coded\
+#print('OPTIONS:')
+fuzz.add_argument('--custom-auth=string',help='Signal that the fuzzer should use hard-coded\
         authentication for a specific application (e.g. DVWA).') #OPTIONAL
 
-cmds.print_help()
-ops.print_help()
+#print('Discover options:')
+fuzz.add_argument('--common-words=file', help='Newline-delimited file of common words to be \
+        used in page guessing and input guessing.')
 
+
+
+fuzz.print_help()
