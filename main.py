afbbf664bd30851af5183cb38552d9a3fba5c362
Ryan Switzer <rjs4244@rit.edu>
2014-09-20 16:48:05 -0400

    Fixes made to structure

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ INIT = 'fuzz'
 COMMANDS = ['discover']#ADD test
 OPTIONS = ['--custom-auth','--common-words']#OPTIONAL --custom-auth 
 
+def main():
+    fuzz = argparse.ArgumentParser(description='fuzz [discover | test] url OPTIONS', add_help=False)
+
+    #COMMANDS:
+    fuzz.add_argument('discover',help='Output a comprehensive,human-readable list of all \
+            discovered inputs to the system. Techniques include both crawling and guessing.')
+    fuzz.add_argument('test',help='Discover all inputs, then attempt a list of exploit vectors\
+            on those inputs. Report potential vulnerabilities.')
+    
+    #OPTIONS:
+    fuzz.add_argument('--custom-auth=string',help='Signal that the fuzzer should use hard-coded\
+            authentication for a specific application (e.g. DVWA).') #OPTIONAL TASK
+    
+    #Discover options:
+    fuzz.add_argument('--common-words=file', help='Newline-delimited file of common words to be \
+            used in page guessing and input guessing.')
+    
+    fuzz.print_help()
+    
+    for cmd in sys.stdin:
+        cmd = cmd.split()
+        process(cmd)
+
+
 def process(cmd):
     if (cmd[0].lower() == INIT) & (cmd[1].lower() in COMMANDS):
         print(cmd[0])       #fuzz
@@ -18,28 +42,9 @@ def process(cmd):
         print ('Error command not supported.\n')
     return
 
-
-fuzz = argparse.ArgumentParser(description='fuzz [discover | test] url OPTIONS', add_help=False)
-
-#COMMANDS:
-fuzz.add_argument('discover',help='Output a comprehensive,human-readable list of all \
-        discovered inputs to the system. Techniques include both crawling and guessing.')
-fuzz.add_argument('test',help='Discover all inputs, then attempt a list of exploit vectors\
-        on those inputs. Report potential vulnerabilities.')
-
-#OPTIONS:
-fuzz.add_argument('--custom-auth=string',help='Signal that the fuzzer should use hard-coded\
-        authentication for a specific application (e.g. DVWA).') #OPTIONAL TASK
-
-#Discover options:
-fuzz.add_argument('--common-words=file', help='Newline-delimited file of common words to be \
-        used in page guessing and input guessing.')
+main()
 
 
-fuzz.print_help()
 
-for cmd in sys.stdin:
-    cmd = cmd.split()
-    process(cmd)
 
 
