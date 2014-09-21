c1888170e617ee41c24ea00d87c652515e21ce7c
Alex Parrill <initrd.gz@gmail.com>
2014-09-21 16:22:35 -0400

    Replaced old main.py with main2.py

--- main.py
+++ main.py
@@ -1,50 +1,88 @@
-import requests
-import argparse
+
 import sys
+import argparse
+import json
+import logging
+logging.basicConfig(format="%(levelname)10s:%(message)s", level=logging.WARNING, stream=sys.stderr)
 
-INIT = 'fuzz'
-COMMANDS = ['discover']#ADD test
-OPTIONS = ['--custom-auth','--common-words']#OPTIONAL --custom-auth 
+from crawler import Site
 
-def main():
-    fuzz = argparse.ArgumentParser(description='fuzz [discover | test] url OPTIONS', add_help=False)
+def parse_args():
+	parser = argparse.ArgumentParser()
+	subparsers = parser.add_subparsers(title="Subcommands", dest="command")
+	
+	discover_parser = subparsers.add_parser("discover",help="Discovers inputs to the website")
+	test_parser = subparsers.add_parser("test", help="Not implemented yet")
+	
+	parser.add_argument("--custom-auth", dest="customauth", help="Custom authentication string. This should be a JSON-encoded string of POST parameters to pass to the first URL.", default=None)
+	
+	log_levels = parser.add_mutually_exclusive_group()
+	log_levels.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="Output verbose debugging info", default=False)
+	log_levels.add_argument("-q", "--quiet", action="store_true", dest="quiet", help="Do not output anything except errors and results", default=False)
+	
+	discover_parser.add_argument("url", help="URL to start crawling at")
+	discover_parser.add_argument("--common-words", dest="commonwords", required=True, help="File of common words to use in input and URL guessing", type=argparse.FileType())
 
-    #COMMANDS:
-    fuzz.add_argument('discover',help='Output a comprehensive,human-readable list of all \
-            discovered inputs to the system. Techniques include both crawling and guessing.')
-    fuzz.add_argument('test',help='Discover all inputs, then attempt a list of exploit vectors\
-            on those inputs. Report potential vulnerabilities.')
-    
-    #OPTIONS:
-    fuzz.add_argument('--custom-auth=string',help='Signal that the fuzzer should use hard-coded\
-            authentication for a specific application (e.g. DVWA).') #OPTIONAL TASK
-    
-    #Discover options:
-    fuzz.add_argument('--common-words=file', help='Newline-delimited file of common words to be \
-            used in page guessing and input guessing.')
-    
-    fuzz.print_help()
-    
-    for cmd in sys.stdin:
-        cmd = cmd.split()
-        process(cmd)
+	args = parser.parse_args()
+	if "command" not in vars(args) or not args.command:
+		parser.print_help()
+		sys.exit(1)
+	return args
 
+def cmd_test(args):
+	print("test command not supported yet")
+	sys.exit(1)
 
-def process(cmd):
-    if (cmd[0].lower() == INIT) & (cmd[1].lower() in COMMANDS):
-        print(cmd[0])       #fuzz
-        print(cmd[1])       #COMMAND        
-        print(cmd[2])       #URL  
-        element = cmd[3].split('=')
-        print(element[1])   #FILE
-        
-    else:
-        print ('Error command not supported.\n')
-    return
+def cmd_discover(args):
+	if args.verbose:
+		logging.getLogger("crawler").setLevel(logging.DEBUG)
+	elif args.quiet:
+		logging.getLogger("crawler").setLevel(logging.ERROR)
+	else:
+		logging.getLogger("crawler").setLevel(logging.INFO)
+	
+	if args.customauth:
+		try:
+			auth = json.loads(args.customauth)
+		except ValueError as e:
+			print("Invalid custom-auth string:")
+			print(str(e))
+			sys.exit(1)
+	else:
+		auth = None
+	
+	words = list()
+	for word in args.commonwords:
+		words.append(word)
+	args.commonwords.close()
+	
+	crawler = Site(words)
+	crawler.crawl(args.url, auth)
+	
+	for page in crawler.pages.values():
+		if not page.valid:
+			if not page.guessed:
+				print("Couldn't fetch", page.url, "(status code:", page.response_code, ")")
+			continue
+		
+		print(page.url)
+		
+		if page.guessed:
+			print("\t Page URL was guessed.")
+		
+		print("\t", len(page.forms), "form(s)")
+		for form in page.forms:
+			print("\t\t", form.action, form.method)
+			
+			for field in form.fields:
+				print("\t\t\t", field.name, "(", field.type, ")")
+		
+		print("\t", len(page.get_parameters), "GET parameter(s)")
+		for param in page.get_parameters:
+			print("\t\t", param)
 
-main()
-
-
-
-
-
+args = parse_args()
+if args.command == "test":
+	cmd_test(args)
+elif args.command == "discover":
+	cmd_discover(args)
